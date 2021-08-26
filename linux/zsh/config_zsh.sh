461256d04b3e848fbf45b6bc5ed8bd0ac57d6f81
Kobi Caps <capskobi@gmail.com>
2021-08-26 19:57:19 +0000

    Updated config_zsh.sh with correct link to zshrc file

--- linux/zsh/config_zsh.sh
+++ linux/zsh/config_zsh.sh
@@ -20,5 +20,5 @@ touch .aliases
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-curl https://gitlab.com/k-caps/linux-scripts/-/raw/master/zshrc | sed "s|/home/kobi|$HOME|g"  > ~/.zshrc
+curl https://gitlab.com/k-caps/scripts/-/raw/master/linux/zsh/zshrc | sed "s|/home/kobi|$HOME|g"  > ~/.zshrc
 zsh
